e0755fb48c0de690bfc21683b43af23f5a7af607
DaviStudios <112717418+DaviStudios@users.noreply.github.com>
2023-10-22 11:32:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,55 +3,104 @@ variables = {}
 def execute_code(code):
     code = code.strip()
 
-    if code.startswith("var "):
-        var_expr = code[len("var "):]
-        var_name, var_value = var_expr.split(" = ")
+    if code == "exit":
+        return False
+    elif code.startswith("run(") and code.endswith(")"):
+        file_expr = code[len("run("):-1].strip()
+        if file_expr.endswith(".davk"):
+            try:
+                with open(file_expr, "r") as file:
+                    for line in file:
+                        result = execute_code(line.strip())
+                        if result is not None:
+                            print(result)
+            except FileNotFoundError:
+                print(f"File '{file_expr}' not found")
+        else:
+            print(f"Invalid file extension. Only '.davk' files are supported.")
+    elif code.startswith("var int "):
+        var_expr = code[len("var int "):]
+        var_name, var_value = var_expr.split(" = ", 1)
         variables[var_name.strip()] = int(var_value.strip())
-        print(f"var {var_name.strip()} made")
+        return f"Integer variable {var_name.strip()} set to {var_value.strip()}"
+    elif code.startswith("var str "):
+        var_expr = code[len("var str "):]
+        var_name, var_value = var_expr.split(' = "', 1)
+        var_value = var_value.rstrip('"')
+        variables[var_name.strip()] = var_value
+        return f"String variable {var_name.strip()} set to '{var_value}'"
     elif code.startswith("console("):
         message = code[len("console("):-1].strip()
         if message.startswith('"') and message.endswith('"'):
-            print(message[1:-1])
+            return message[1:-1]
         elif message in variables:
-            print(variables[message])
+            return variables[message]
         else:
-            print("Undefined: ", message)
-    elif code.startswith("read("):
+            try:
+                result = eval(message, variables)
+                return str(result)
+            except Exception as e:
+                return "Error: invalid"
+    elif code.startswith("read(") and code.endswith(")"):
         file_expr = code[len("read("):-1].strip()
         try:
             with open(file_expr, "r") as file:
-                file_content = file.read()
-                print(file_expr)
-                print(file_content)
+                content = file.read()
+                if content.startswith('"') and content.endswith('"'):
+                    return content[1:-1]
+                else:
+                    return content
         except FileNotFoundError:
-            print(f"File '{file_expr}' not found.")
+            return f"File '{file_expr}' not found"
     elif code.startswith("if "):
         condition, then_part = code[len("if "):].split(" then ", 1)
         if eval(condition, variables):
-            execute_code(then_part)
-            print("done")
-    elif code.startswith("print("):
+            then_part = then_part.strip()
+            if then_part.startswith("var int "):
+                var_expr = then_part[len("var int "):]
+                var_name, var_value = var_expr.split(" = ", 1)
+                variables[var_name.strip()] = int(var_value.strip())
+                return f"Integer variable {var_name.strip()} set to {var_value.strip()}"
+            elif then_part.startswith("var str "):
+                var_expr = then_part[len("var str "):]
+                var_name, var_value = var_expr.split(' = "', 1)
+                var_value = var_value.rstrip('"')
+                variables[var_name.strip()] = var_value
+                return f"String variable {var_name.strip()} set to '{var_value}'"
+            else:
+                result = execute_code(then_part)
+                if not result.startswith("Error: invalid"):
+                    return result
+                else:
+                    return ""
+    elif code.startswith("print(") and code.endswith(")"):
         message = code[len("print("):-1]
         if message in variables:
-            print(variables[message])
+            return variables[message]
         else:
-            print(message)
-    elif code.startswith("file_write("):
-        file_expr = code[len("file_write("):-1]
-        file_name, file_content = file_expr.split(" = ")
+            return message
+    elif code.startswith("file_write(") and code.endswith(")"):
+        file_expr = code[len("file_write("):-1].strip()
+        file_name, file_content = file_expr.split(" = ", 1)
         with open(file_name.strip(), "w") as file:
             file.write(file_content.strip())
-    elif code.startswith("file_read("):
-        file_name = code[len("file_read("):-1]
-        with open(file_name.strip(), "r") as file:
-            content = file.read()
-            print(content)
+    elif code.startswith("file_read(") and code.endswith(")"):
+        file_name = code[len("file_read("):-1].strip()
+        try:
+            with open(file_name, "r") as file:
+                content = file.read()
+                if content.startswith('"') and content.endswith('"'):
+                    return content[1:-1]
+                else:
+                    return content
+        except FileNotFoundError:
+            return f"File '{file_name}' not found"
     else:
         try:
             result = eval(code, variables)
-            print(result)
+            return str(result)
         except Exception as e:
-            print("Error:", e)
+            return "Error: invalid"
 
 while True:
     try:
@@ -61,4 +110,6 @@ while True:
 
     if user_input == "exit":
         break
-    execute_code(user_input)
+    result = execute_code(user_input)
+    if result is not None:
+        print(result)
